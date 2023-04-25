2d01ebf91d1403b679e712a2bb71adaf84d32b12
WeebNinja <Nattaponjaijina2002@gmail.com>
2023-04-25 13:06:13 +0700

    Test

---
+++ src/componenets/Footer/index.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss'
+
+type Props = {}
+
+const Footer = ({}: Props) => {
+  return (
+    <footer>
+        <p>
+            @Copyright 2023 New Nattapon
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
